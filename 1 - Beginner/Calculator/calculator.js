8a351e6fb9e622c6a8da0367ddd4d6c0e03647f1
datomavic <datomavic@gmail.com>
2021-11-05 05:12:35 -0600

    Started Calculator
    Added HTML Markup/CSS for calculator mini-project.

---
+++ 1 - Beginner/Calculator/calculator.js
@@ -0,0 +1,17 @@
+
+
+function add(operand){
+  return this + operand;
+}
+
+function subtract(operand){
+  return this - operand;
+}
+
+function divide(operand){
+  return this / operand;
+}
+
+function equals(){}
+function clear(){}
+function clearAll(){}
