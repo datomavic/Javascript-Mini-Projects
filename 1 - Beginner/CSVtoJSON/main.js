0b69dc31454fd427cc7105dbb147ba0b380b53e9
datomavic <datomavic@gmail.com>
2021-11-15 06:12:26 -0700

    Updated CSVtoJSON
    -Added animated transition for copy-to-clipboard button.
    
    -Added JSON to CSV functionality
    
    -Minor HTML & CSS changes
    
    Still need to implement:
    
    -Upload file functionality
    -Polishing

--- 1 - Beginner/CSVtoJSON/main.js
+++ 1 - Beginner/CSVtoJSON/main.js
@@ -1,11 +1,18 @@
-//Input and output textarea
+//Input and output textarea elements
 let input = document.querySelector("#input-content");
 let output = document.querySelector("#output-content");
+
 //Booleans to keep track of current data type
 let isCSV = false;
 let isJSON = false;
+
 //Boolean for keeping track of converted or not.
 let converted = false;
+
+//Global variables for storing converted data
+let convertedJSON;
+let convertedCSV;
+
 //Add event listener to every button
 document.querySelectorAll("button").forEach((element) => {
   element.addEventListener("click", onClickProcess);
@@ -43,16 +50,20 @@ function upload(){
  * input textarea.
  */
 function convert(content = input.value){
+  //Trim out any beginning/trailing spaces
+  content = content.trim();
   if(content.length == 0)
     return;
 
+  //If our content string begins with { or [, we know it's possible JSON format
   if(content[0] == "{" || content[0] == "["){
     isJSON = true;
-    isCsv = false;
+    isCSV = false;
     jsonToCsv(content);
   }
+  //Otherwise it's a possible CSV format.
   else{
-    isCsv = true;
+    isCSV = true;
     isJSON = false;
     csvToJson(content);
   }
@@ -65,9 +76,9 @@ function convert(content = input.value){
  * @param {string} content - a string in .csv format to be converted to JSON
  */
 function csvToJson(content){
-  isCSV = true;
   const quoteChar = `"`;
   const delimiter = `,`;
+  //Regex to account for possible non-delimeter commas within our CSV.
   const regex = new RegExp(`\\s*(${quoteChar})?(.*?)\\1\\s*(?:${delimiter}|$)`, 'gs');
   const match = content => [...content.matchAll(regex)].map(match => match[2])
     .filter((_, i, a) => i < a.length - 1); // cut off blank match at end
@@ -80,8 +91,9 @@ function csvToJson(content){
     [heads[i] || `extra_${i}`]: (cur.length > 0) ? (Number(cur) || cur) : null
   }), {}));
 
-  let outputText = JSON.stringify(json, null, 1);
-  output.innerText = outputText;
+  //Conversion complete. Stringify our JSON to output to output display
+  let convertedJSON = JSON.stringify(json, null, 1);
+  output.value = convertedJSON;
   converted = true;
 }
 
@@ -92,7 +104,63 @@ function csvToJson(content){
  * @param {string} content - a string in JSON format to be converted to CSV
  */
 function jsonToCsv(content){
+  let json;
+  //Attempt to parse 'content' into JSON object
+  try{
+    json = JSON.parse(content);
+  }
+  catch(err){
+    output.innerText = err.message;
+  }
 
+  //Headers will be our CSV's 'row titles'. Use a Set to prevent duplication.
+  let headers = new Set();
+  let values = [];
+  //If json is a single object, rather than array of objects
+  if(!Array.isArray(json)){
+    json = [json];
+  }
+
+  //Gather 'headers' from each object's keys.
+  //Push a new array into 'values' (represents each row in CSV)
+  for(object of json){
+    for(key of Object.keys(object))
+      headers.add(key);
+    values.push([]);
+  }
+
+  //Start gathering CSV 'values' from object's properties via its 'header'. 
+  //Push this value into appropriate sub-array
+  let i = 0;
+  for(object of json){
+    for(title of headers){
+      if(object[title] == undefined){
+        values[i].push("");
+      }
+      else
+        values[i].push(object[title]);
+    }
+    i++;
+  }
+
+  //Print to convertedCSV our 'headers'.
+  let convertedCSV = "";
+  for(header of headers){
+    convertedCSV += ""+header+",";
+  }
+  convertedCSV += "\n"
+
+  //Begin printing our values to convertedCSV. 
+  for(let i = 0; i < values.length; i++){ //Rows
+    for(value of values[i]){ //Columns
+      convertedCSV += ""+value+",";
+    }
+    convertedCSV += "\n";
+  }
+
+  //Conversion complete. Output to our output display.
+  output.value = convertedCSV;
+  converted = true;
 }
 
 /**
@@ -105,19 +173,24 @@ function jsonToCsv(content){
  * to be downloaded.
  */
 function download(){
-  if(converted == false)
+  if(converted == false || output.value == "")
     return;
+  //Get file type based on isJSON boolean
   let type = isJSON == true ? 'text/csv' : 'application/json';
+  let extension = type.split('/')[1];
   type = `${type};charset=utf-8`;
-  let fileName = "converted."+type.split(';')[0];
+  let fileName = "converted."+extension;
+  //Create the file with Blob, sending in output value, file type
   let blob = new Blob([output.value], {type})
   a = document.createElement('a');
+  //Create URL link for our file
   url = URL.createObjectURL(blob);
   a.href = url;
+  //Set download to be file's name
   a.download = fileName;
   document.body.appendChild(a);
 
-
+  //If user selects "yes", click the anchor tag under-the-hood.
   if(confirm("Are you sure you'd like to download your converted file?"))
     a.click();
 }
@@ -130,9 +203,67 @@ function download(){
  * textarea and copy it to user's clipboard.
  */
 function copy(){
-  if(output.value.length > 0){
-    navigator.clipboard.writeText(output.value);
+  if(output.value.length == 0 || copy.running)
+    return;
+  //To avoid code from executing twice asynchronously
+  copy.running = true;
+  //Element selectors for copy-button icons
+  let icon = document.querySelector("#copy-icon");
+  let button = document.querySelector("#copy-button");
+  let text = document.createElement("p");
+  text.style.marginBottom = "0";
+  text.style.color = "#3a3a3a";
+  text.style.display = "none";
+  text.innerText = "Copied!";
+  button.appendChild(text);
+
+  //Copy output value to clipboard
+  navigator.clipboard.writeText(output.value);
+
+  //Fade in transition for element
+  let fade = function(element){
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = "none";
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 3);
   }
+  
+  //Fade out transition for element
+  let unfade = function(element){
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 2);
+  }
+
+  //Fade out copy icon, begin fading in 'Copied!'
+  fade(icon);
+
+  setTimeout(() => {
+    unfade(text);
+  }, 355);
+
+  setTimeout(() => {
+    fade(text);
+  }, 1500);
+
+  setTimeout(() => {
+    unfade(icon);
+    //Once this asynchronous call is done, set copy.running to false.
+    copy.running = false;
+  }, 1800);
 }
 
 
