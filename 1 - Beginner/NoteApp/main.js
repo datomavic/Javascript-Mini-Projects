cbe917139e2b00655937cd0ab07dbbfd2c4aadb9
datomavic <datomavic@gmail.com>
2021-11-25 21:12:07 -0700

    Added notes.js to NotesApp
    Added notes.js which contains our Notes 'class' blueprint.

---
+++ 1 - Beginner/NoteApp/main.js
@@ -0,0 +1,1 @@
+let notes = new Notes();
---
+++ 1 - Beginner/NoteApp/notes.js
@@ -0,0 +1,51 @@
+class Notes{
+  #content;
+
+  constructor(){
+    this.#content = null;
+  }
+
+  /**
+   * @memberof Notes
+   * Will copy the content of the note object to the users clipboard.
+   */
+  copy(){
+
+  }
+
+  /**
+   * @memberof Notes
+   * Will delete the content of the notes object.
+   */
+  delete(){
+
+  }
+
+  /**
+   * @memberof Notes
+   * Will save the content of the notes object as a .txt file
+   */
+  save(){
+
+  }
+
+  /**
+   * @memberof Notes
+   * This is a getter for our #content field, the text within our
+   * notes object. 
+   * @return {String} a string of our this Notes #content.
+   */
+  get content(){
+    return this.#content;
+  }
+
+  /**
+   * @memberof Notes
+   * This is a setter for our #content field.
+   * @param {String} data - a string to be set to this Notes
+   * #content field.
+   */
+  set content(data){
+    this.#content = data;
+  }
+}
