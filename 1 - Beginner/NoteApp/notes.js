8153ac56250bfd718a531ed3288c982b3aea7494
datomavic <datomavic@gmail.com>
2021-11-26 00:28:01 -0700

    Updated NotesApp

--- 1 - Beginner/NoteApp/notes.js
+++ 1 - Beginner/NoteApp/notes.js
@@ -10,7 +10,66 @@ class Notes{
    * Will copy the content of the note object to the users clipboard.
    */
   copy(){
+    if(this.#content.length == 0 || copy.running)
+      return;
+    //Function property to avoid code from executing twice asynchronously
+    copy.running = true;
+    //Element selectors for copy-button icons
+    let icon = document.querySelector("#copy-icon");
+    let button = document.querySelector("#copy-button");
+    let text = document.createElement("p");
+    text.style.marginBottom = "0";
+    text.style.color = "#3a3a3a";
+    text.style.display = "none";
+    text.innerText = "Copied!";
+    button.appendChild(text);
 
+    //Copy output value to clipboard
+    navigator.clipboard.writeText(output.value);
+    //Fade in transition for element
+    let fade = function(element){
+      var op = 1;  // initial opacity
+      var timer = setInterval(function () {
+          if (op <= 0.1){
+              clearInterval(timer);
+              element.style.display = "none";
+          }
+          element.style.opacity = op;
+          element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+          op -= op * 0.1;
+      }, 3);
+    }
+    
+    //Fade out transition for element
+    let unfade = function(element){
+      var op = 0.1;  // initial opacity
+      element.style.display = 'block';
+      var timer = setInterval(function () {
+          if (op >= 1){
+              clearInterval(timer);
+          }
+          element.style.opacity = op;
+          element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+          op += op * 0.1;
+      }, 2);
+  }
+
+  //Fade out copy icon, begin fading in 'Copied!'
+  fade(icon);
+
+  setTimeout(() => {
+    unfade(text);
+  }, 355);
+
+  setTimeout(() => {
+    fade(text);
+  }, 1500);
+
+  setTimeout(() => {
+    unfade(icon);
+    //Once this asynchronous call is done, set copy.running to false.
+    copy.running = false;
+  }, 1800);
   }
 
   /**
