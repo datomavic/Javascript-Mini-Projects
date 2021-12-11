507ecc27933dd700a1af86e57f29a691e265ad89
datomavic <datomavic@gmail.com>
2021-12-11 01:31:35 -0700

    Updated Pomodoro

--- 1 - Beginner/Pomodoro/main.js
+++ 1 - Beginner/Pomodoro/main.js
@@ -1,1 +1,7 @@
 import PomodoroGrid from './PomodoroGrid.js';
+
+let gridContainer = document.querySelector('#grid-container');
+let copy = gridContainer.querySelector('.box');
+for(let i = 0; i < 2400; i++){  
+  gridContainer.appendChild(copy.cloneNode(true));
+}
