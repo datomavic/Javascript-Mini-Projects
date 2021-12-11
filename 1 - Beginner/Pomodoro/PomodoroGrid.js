5ee5cd74769a034fc39da5def530e9184d025162
datomavic <datomavic@gmail.com>
2021-12-11 03:03:46 -0700

    Updated Pomodoro

--- 1 - Beginner/Pomodoro/PomodoroGrid.js
+++ 1 - Beginner/Pomodoro/PomodoroGrid.js
@@ -1,5 +1,59 @@
 export default class PomodoroGrid{
-  constructor(){
+  /**
+   * @memberof PomodoroGrid
+   * @param {Object} container - Grid's container DOM element 
+   * @param {Integer} size - the size of our PomodoroGrid 
+   */
+  constructor(container, size){
+    //Constructor fields
+    this.container = container;
+    this.size = size;
+    //Additional fields for our grid class.
+    this.pomArray;
+    this.counter;
+    //Initialize our grid with these fields/properties.
+    this.initialize.call(this);
+  }
 
+  /**
+   * Will initialize a new grid on the document based on the current this.size. Will set this.counter to 0.
+   * @memberof PomodoroGrid
+   */
+  initialize(){
+    this.pomArray = [];
+    this.counter = 0;
+    while(this.container.firstChild)
+      this.container.removeChild(this.container.firstChild);
+    const boxElement = document.createElement('div');
+    boxElement.className = 'box';
+    for(let i = 0; i < this.size; i++){
+      const clone = boxElement.cloneNode(true);
+      clone.id = i;
+      this.container.appendChild(clone);
+      this.pomArray.push(false);
+    }
+  }
+
+  /**
+   * This will add a new tomato to our grid.
+   * @memberof PomodoroGrid 
+   */
+  addTomato(){
+    const tomato = document.createElement('img');
+    tomato.src = './tomato.png';
+    document.getElementById(this.counter).appendChild(tomato);
+    this.pomArray[this.counter] = true;
+    this.counter++;
+    console.log(this.pomArray);
+  }
+
+  /**
+   * When called, will re-initialize the size of the Pomodoro Grid.
+   * @memberof PomodoroGrid
+   * @param Integer size - size of grid
+   */
+  setSize(size){
+    this.size = size;
+    this.initialize.call(this);
   }
 }
--- 1 - Beginner/Pomodoro/main.js
+++ 1 - Beginner/Pomodoro/main.js
@@ -1,7 +1,10 @@
 import PomodoroGrid from './PomodoroGrid.js';
 
-let gridContainer = document.querySelector('#grid-container');
-let copy = gridContainer.querySelector('.box');
-for(let i = 0; i < 2400; i++){  
-  gridContainer.appendChild(copy.cloneNode(true));
-}
+const gridContainer = document.querySelector('#grid-container');
+const grid = new PomodoroGrid(gridContainer, 240);
+
+
+// let copy = gridContainer.querySelector('.box');
+// for(let i = 0; i < 2400; i++){  
+//   gridContainer.appendChild(copy.cloneNode(true));
+// }
